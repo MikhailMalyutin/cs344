f57167322e767f725d7c2904eee49868e9c21405
mmalutin <mmalutin@flexis.ru>
2015-07-29 12:36:30 +0300

    Implementation

--- prototypes/src/Prototype.java
+++ prototypes/src/Prototype.java
@@ -2,37 +2,39 @@ public class Prototype {
     private static int maxThreads = 4;
 
     private static void scanReduceForBlock(int d_res[],
-                                       int size, int myId) {
+                                       int size) {
         int prevId;
         int prevValue;
         int myValue;
 
         for (int s = 2; s <= size; s *= 2) {
-            prevId    = myId - s/2;
-            prevValue = (myId >= s/2) ? d_res[prevId] : 0;
-            myValue   = d_res[myId];
-            if (((myId+1) % s) == 0 && (myId >= s/2)) {
-                d_res[myId] = myValue + prevValue;
+            for (int myId = 0; myId < size; ++ myId) {
+                prevId = myId - s / 2;
+                prevValue = (myId >= s / 2) ? d_res[prevId] : 0;
+                myValue = d_res[myId];
+
+                if (((myId + 1) % s) == 0 && (myId >= s / 2)) {
+                    d_res[myId] = myValue + prevValue;
+                }
             }
         }
     }
 
-    private static void scanDownStepForBlock(int d_res[], int initialS, int myId) {
+    private static void scanDownStepForBlock(int d_res[], int initialS) {
         int prevId;
         int prevValue;
         int myValue;
 
-        if (myId >=initialS) {
-            return;
-        }
+        for (int myId = 0; myId < maxThreads; ++ myId) {
 
-        for (int s = initialS; s >= 2; s /= 2) {
-            prevId = myId - s / 2;
-            prevValue = (myId >= s/2) ? d_res[prevId] : 0;
-            myValue = d_res[myId];
-            if (((myId+1) % s)  == 0 && myId >= s/2) {
-                d_res[prevId] = myValue;
-                d_res[myId] = myValue + prevValue;
+            for (int s = initialS; s >= 2; s /= 2) {
+                prevId = myId - s / 2;
+                prevValue = (myId >= s / 2) ? d_res[prevId] : 0;
+                myValue = d_res[myId];
+                if (((myId + 1) % s) == 0 && myId >= s / 2) {
+                    d_res[prevId] = myValue;
+                    d_res[myId] = myValue + prevValue;
+                }
             }
         }
 
@@ -47,20 +49,22 @@ public class Prototype {
                 return;
             }
             d_res[myId] = d_in[myId];
+        }
 
-            scanReduceForBlock(d_res, Math.min(maxThreads, size), myId);
-            d_res[size - 1] = 0;
+        scanReduceForBlock(d_res, Math.min(maxThreads, size));
+        d_res[size - 1] = 0;
 
-            int compactRatio = size / maxThreads;
-            if (compactRatio > 1) {
-                if (myId < maxThreads) {
-                    sdata[myId] = d_res[myId * maxThreads + maxThreads - 1];
-                    scanReduceForBlock(sdata, maxThreads, myId);
-                    //scanReduceForBlock(sdata, maxThreads/2, compactedMyId);
-                    sdata[compactRatio - 1] = 0;
-                    scanDownStepForBlock(sdata, maxThreads, myId);
-                    d_res[myId * maxThreads + maxThreads - 1] = sdata[myId];
-                }
+        int compactRatio = size / maxThreads;
+        if (compactRatio > 1) {
+            for (int myId = 0; myId < maxThreads; ++myId) {
+                sdata[myId] = d_res[myId * maxThreads + maxThreads - 1];
+            }
+            scanReduceForBlock(sdata, maxThreads);
+            //scanReduceForBlock(sdata, maxThreads/2, compactedMyId);
+            sdata[compactRatio - 1] = 0;
+            scanDownStepForBlock(sdata, maxThreads);
+            for (int myId = 0; myId < maxThreads; ++myId) {
+                d_res[myId * maxThreads + maxThreads - 1] = sdata[myId];
             }
         }
     }
