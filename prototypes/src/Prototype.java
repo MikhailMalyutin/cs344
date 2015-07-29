33afca762b7ea4c0f9b50f2db43f889d1ec57898
mmalutin <mmalutin@flexis.ru>
2015-07-29 13:46:50 +0300

    Implementation

--- prototypes/src/Prototype.java
+++ prototypes/src/Prototype.java
@@ -2,12 +2,12 @@ public class Prototype {
     private static int maxThreads = 8;
 
     private static void scanReduceForBlock(int d_res[],
-                                       int size) {
+                                           int initialS, int size) {
         int prevId;
         int prevValue;
         int myValue;
 
-        for (int s = 2; s <= size; s *= 2) {
+        for (int s = 2; s <= initialS; s *= 2) {
             for (int myId = 0; myId < size; ++ myId) {
                 prevId = myId - s / 2;
                 prevValue = (myId >= s / 2) ? d_res[prevId] : 0;
@@ -17,15 +17,16 @@ public class Prototype {
                     d_res[myId] = myValue + prevValue;
                 }
             }
+            System.out.print(d_res);
         }
     }
 
-    private static void scanDownStepForBlock(int d_res[], int initialS) {
+    private static void scanDownStepForBlock(int d_res[], int initialS, int maxSize) {
         int prevId;
         int prevValue;
         int myValue;
 
-        for (int myId = 0; myId < maxThreads; ++ myId) {
+        for (int myId = 0; myId < maxSize; ++ myId) {
 
             for (int s = initialS; s >= 2; s /= 2) {
                 prevId = myId - s / 2;
@@ -51,7 +52,7 @@ public class Prototype {
             d_res[myId] = d_in[myId];
         }
 
-        scanReduceForBlock(d_res, Math.min(maxThreads, size));
+        scanReduceForBlock(d_res, Math.min(maxThreads, size), size);
         d_res[size - 1] = 0;
 
         int compactRatio = size / maxThreads;
@@ -59,10 +60,10 @@ public class Prototype {
             for (int myId = 0; myId < compactRatio; ++myId) {
                 sdata[myId] = d_res[myId * maxThreads + maxThreads - 1];
             }
-            scanReduceForBlock(sdata, maxThreads);
+            scanReduceForBlock(sdata, maxThreads, maxThreads);
             //scanReduceForBlock(sdata, maxThreads/2, compactedMyId);
             sdata[compactRatio - 1] = 0;
-            scanDownStepForBlock(sdata, maxThreads);
+            scanDownStepForBlock(sdata, maxThreads, maxThreads);
             for (int myId = 0; myId < compactRatio; ++myId) {
                 d_res[myId * maxThreads + maxThreads - 1] = sdata[myId];
             }
@@ -85,7 +86,7 @@ public class Prototype {
                 0,
                 1,
                 1,
-                1,
+                1/**,
                 0,
                 0,
                 1,
@@ -101,9 +102,11 @@ public class Prototype {
                 0,
                 0,
                 1,
-                1};
+                1**/
+        };
         int outData[] = new int[inData.length];
         blellochScan(inData, outData, inData.length);
+        scanDownStepForBlock(outData, maxThreads, inData.length);
         System.out.println(outData);
     }
 }
